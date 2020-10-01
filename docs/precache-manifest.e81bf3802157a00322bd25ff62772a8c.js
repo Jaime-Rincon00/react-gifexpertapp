f1c6d3a8a7514cc54cecb4ac3d18981ce3b59f36
JAIME ALBERTO RINCON CASTILLO <jaime-rincon@outlook.com>
2020-10-01 15:23:15 -0500

    docs add

---
+++ docs/precache-manifest.e81bf3802157a00322bd25ff62772a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988fe906d682e413434c776ffeb564af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53cc6a781bb340053380",
+    "url": "/static/css/main.9977d1c1.chunk.css"
+  },
+  {
+    "revision": "b8d5c62503bfa23a4e97",
+    "url": "/static/js/2.99e04cde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.99e04cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53cc6a781bb340053380",
+    "url": "/static/js/main.e0116881.chunk.js"
+  },
+  {
+    "revision": "722a304d2cb62627ceb8",
+    "url": "/static/js/runtime-main.3cc8bee4.js"
+  }
+]);
